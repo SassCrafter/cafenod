10134d77ee6a66b7b4739b0bd09158341328ff43
Dima <webbootcamp2020@gmail.com>
2021-04-15 22:08:53 +0300

    Fixed desktop nav, and button import path

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Buttons } from "./Buttons/Buttons";
-export { default as Button } from "./Buttons/Button/Button";
+export { default as Buttons } from "./UI/Buttons/Buttons";
+export { default as Button } from "./UI/Buttons/Button/Button";
 export { default as Logo } from "./UI/Logo/Logo";
 export { default as Section } from "./UI/Section/Section";
 export { default as Header } from "./Header/Header";
